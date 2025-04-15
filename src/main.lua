dc1811104f863dd51c2b64dc97d84b5346dd95a5
Terra_Verbis <protocolArtwork@gmail.com>
2025-04-15 17:21:51 -0400

    Improved error handling

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,10 @@
-require 'luarocks.loader'
+require("luarocks.loader")
 
 local lfs = require("lfs")
 
 local function file_exists(filename)
-    local attr = lfs.attributes(filename)
-    return attr and attr.mode == "file"
+	local attr = lfs.attributes(filename)
+	return attr and attr.mode == "file"
 end
 
 local FMT_OP = "[%s] %s"
@@ -31,33 +31,68 @@ local function report_err(op, err, op_args)
 end
 
 ---@param dir string
-local function mkdir(dir,...)
-	local op_report = report_op("mkdir", {dir,...})
+---@return boolean, string?
+local function mkdir(dir, ...)
+	local op_report = report_op("mkdir", { dir, ... })
 	local success, value = lfs.mkdir(dir)
 	if not success then
-		local err_report = report_err("mkdir", value, {dir, ...})
+		local err_report = report_err("mkdir", value, { dir, ... })
 		print(err_report)
+		return false, value
 	else
 		print(op_report)
+		return true, nil
 	end
 end
 
----@param dir string 
+---@param dir string
+---@return boolean, string[][]?
 local function proj_skel(dir)
 	local buildDirPath = dir .. "/build"
 	local sourceDirPath = dir .. "/src"
 
+	local errors = {}
+
 	if not file_exists(dir) then
-		mkdir(dir)
+		local s, e = mkdir(dir)
+		if not s then
+			table.insert(errors, { "Could not create base project directory", e })
+		end
 	end
 
 	if not file_exists(buildDirPath) then
-		mkdir(buildDirPath, "(Build directory)")
+		local s, e = mkdir(buildDirPath, "(Build directory)")
+		if not s then
+			table.insert(errors, { "Could not create build directory", e })
+		end
 	end
 
 	if not file_exists(sourceDirPath) then
-		mkdir(sourceDirPath, "(Source directory)")
+		local s, e = mkdir(sourceDirPath, "(Source directory)")
+		if not s then
+			table.insert(errors, { "Could not create source directory", e })
+		end
 	end
+
+	local s = #errors == 0
+	return s, (not s) and errors or nil
 end
 
-proj_skel(arg[1])
+local createSuccess, value = proj_skel(arg[1])
+
+-- PROCESS RESULTS
+print()
+
+if not createSuccess then
+	print("There were errors while creating the project skeleton:")
+
+	---@type string[][]
+	---@diagnostic disable-next-line
+	local eList = value
+
+	for i, v in ipairs(eList) do
+		print('\t'..i, unpack(v))
+	end
+else
+	print("Successful created new project directory")
+end
