a323f18123968d5d31f4949247e1c06be59beff5
Terra_Verbis <protocolArtwork@gmail.com>
2025-04-15 22:25:42 -0400

    Upgraded run.sh to work from any working dir. Removed repl.sh

--- repl.sh
+++
@@ -1,7 +0,0 @@
-if [ -z "$LUA" ]; then
-	LUA="./lua"
-fi
-
-command="$LUA -l lpath -e require'luarocks.loader' -i"
-echo $command
-$command
--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
+# Get the location of this script
+script_dir="$(dirname "$(realpath "$0")")"
+
+# If no lua is defined, use the one defined by luarocks
 if [ -z "$LUA" ]; then
-	LUA="./lua"
+	LUA="$script_dir/lua"
 fi
 
-command="$LUA -l lpath ./lua_modules/share/lua/5.1/main.lua $@"
-echo $command
-$command
+# Get the CPATH and PATH for project-local lua_modules rocktree
+LUA_PATH_EXT="$script_dir/lua_modules/share/lua/5.1/?.lua"
+LUA_CPATH_EXT="$script_dir/lua_modules/lib/lua/5.1/?.so"
+
+# Add project-local lua_modules rocktree to package path & cpath
+lua_code_cpath="package.cpath = package.cpath..';$LUA_CPATH_EXT'"
+lua_code_path="package.path = package.path..';$LUA_PATH_EXT'"
+
+# Run main.lua, prefixed with path changes, passing all arguments to this script
+$LUA -e " $lua_code_path " -e " $lua_code_cpath " $script_dir/lua_modules/share/lua/5.1/main.lua $@
