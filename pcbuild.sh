b6a23caa55fbdd59138b06f68175393e30fe3987
Terra_Verbis <protocolArtwork@gmail.com>
2025-04-15 22:29:32 -0400

    Ensured POSIX compliance

--- pcbuild.sh
+++ pcbuild.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # Get the location of this script
 script_dir="$(dirname "$(realpath "$0")")"
 
 # If no lua is defined, use the one defined by luarocks
 if [ -z "$LUA" ]; then
-	LUA="$script_dir/lua"
+    LUA="$script_dir/lua"
 fi
 
 # Get the CPATH and PATH for project-local lua_modules rocktree
@@ -17,4 +17,4 @@ lua_code_cpath="package.cpath = package.cpath..';$LUA_CPATH_EXT'"
 lua_code_path="package.path = package.path..';$LUA_PATH_EXT'"
 
 # Run main.lua, prefixed with path changes, passing all arguments to this script
-$LUA -e " $lua_code_path " -e " $lua_code_cpath " $script_dir/lua_modules/share/lua/5.1/main.lua $@
+exec $LUA -e "$lua_code_path" -e "$lua_code_cpath" "$script_dir/lua_modules/share/lua/5.1/main.lua" "$@"
